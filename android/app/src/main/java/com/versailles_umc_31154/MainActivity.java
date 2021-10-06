8032293a32d6cbc728f3ed21d8e6938fe17a0523
Crowdbotics <team@crowdbotics.com>
2021-10-06 12:50:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/versailles_umc_31154/MainActivity.java
@@ -0,0 +1,15 @@
+package com.versailles_umc_31154;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "versailles_umc_31154";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "versailles_umc_31154",
+  url: "https://versailles_umc_31154.botics.co",
+  api: "https://versailles_umc_31154.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
